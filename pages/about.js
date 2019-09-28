fc89e067563644ceb87f48dbc91923c51012c210
arcav <armandojcastro84@gmail.com>
2019-09-28 07:25:21 -0400

    Ejemplo Basico de Nextjs

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Layout from '../components/MyLayout';
+
+function About (){
+    return(
+        <div>
+            <Layout>
+              <p> This is About Page</p>
+            </Layout>
+            
+        </div>
+    );
+}
+
+export default About;
